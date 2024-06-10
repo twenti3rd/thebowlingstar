04a0281bcc99e9e83c43ded3f29c8d8c971dd281
Lisa <lisa@rockmoongames.io>
2024-06-10 08:04:42 +0700

    Add a fifth, a sixth and a seventh test

---
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGame2Test.java
@@ -0,0 +1,22 @@
+class JavaGame2Test {
+
+    @Test
+    public void lisaTestFour(){
+
+    }
+
+    @Test
+    public void lisaTestFive(){
+        
+    }
+
+    @Test
+    public void lisaTestSix(){
+        
+    }
+
+    @Test
+    public void lisaTestSeven(){
+        
+    }
+}
